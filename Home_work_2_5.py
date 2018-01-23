7914ec2e587b2f49f36238f58ec70c76f7ee9ee5
Stanislav Shilin <StanislavShilin@yandex.ru>
2018-01-23 23:52:24 +0300

    Д/З 2.5

--- Home_work_2_5.py
+++ Home_work_2_5.py
@@ -1,12 +1,12 @@
 import os
 import subprocess
 
-folder_with_photo = "E:\Phyton\Материалы\Photo_2_5";
-folder_with_result_photo = "E:\Phyton\Материалы\Result";
+folder_with_photo = "E:\Phyton\Materials\Photo_2_5";
+folder_with_result_photo = "E:\Phyton\Materials\Result";
 list_of_photo = os.listdir(folder_with_photo)
 for photo in list_of_photo:
     way_to_photo = os.path.join(folder_with_photo, photo)
     file_extension = os.path.splitext(way_to_photo)
-    if file_extension[1] == '.jpg':
-        process = subprocess.run("convert {} -resize 200 {}".format(way_to_photo, os.path.join(folder_with_result_photo, file_extension[0] +
-                                                                                               "Resize" + file_extension[1])))
+    if file_extension[1].lower() == '.jpg':
+        way_to_result_photo = os.path.join(folder_with_photo, file_extension[0] + "Resize" + file_extension[1])
+        process = subprocess.run("convert {0} {1}".format(way_to_photo, way_to_result_photo))
