a97d7bb5454eb3c9d90cdb162daed1cd8cef8338
Stanislav Shilin <StanislavShilin@yandex.ru>
2018-01-24 00:05:52 +0300

    Д/З 2.5

--- Home_work_2_5.py
+++ Home_work_2_5.py
@@ -1,12 +1,18 @@
 import os
 import subprocess
 
-folder_with_photo = "E:\Phyton\Materials\Photo_2_5";
-folder_with_result_photo = "E:\Phyton\Materials\Result";
-list_of_photo = os.listdir(folder_with_photo)
+folder_with_photo = "Source";
+folder_with_result_photo = "Result";
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+way_to_folder_with_photo = os.path.join(current_dir, folder_with_photo)
+way_to_folder_with_result_photo = os.path.join(current_dir, folder_with_result_photo)
+
+list_of_photo = os.listdir(way_to_folder_with_photo)
 for photo in list_of_photo:
-    way_to_photo = os.path.join(folder_with_photo, photo)
+    way_to_photo = os.path.join(way_to_folder_with_photo, photo)
     file_extension = os.path.splitext(way_to_photo)
     if file_extension[1].lower() == '.jpg':
-        way_to_result_photo = os.path.join(folder_with_photo, file_extension[0] + "Resize" + file_extension[1])
-        process = subprocess.run("convert {0} {1}".format(way_to_photo, way_to_result_photo))
+        way_to_result_photo = os.path.join(way_to_folder_with_result_photo, file_extension[0] + "Resize" + file_extension[1])
+        process = subprocess.run("convert {} -resize 200 {}".format(way_to_photo, way_to_result_photo))
+        print(process)
