a201925a07d86a806b0e7d25bd74f6d8ab51eee3
Stanislav Shilin <StanislavShilin@yandex.ru>
2018-03-19 21:55:25 +0300

    Дипломная работа

--- Diplom.py
+++ Diplom.py
@@ -1,6 +1,8 @@
 import json
 import time
 import os
+import types
+
 import vk
 
 
@@ -8,6 +10,45 @@ def get_api():
     token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
     session = vk.Session(access_token=token)
     result = vk.API(session, v='5.73')
+    return result
+
+
+def get_friends(api, id, fields):
+    try:
+        result = api.friends.get(user_id=id, fields=fields)
+    except Exception as err:
+        if err.code == 6:
+            print("Ждем...")
+            time.sleep(1)
+            result = get_friends(api,id, fields)
+        else:
+            result = err
+    return result
+
+
+def get_groups(api, id, fields):
+    try:
+        result = api.groups.get(user_id=id, extended=1, fields=fields, count=1000)
+    except Exception as err:
+        if err.code == 6:
+            print("Ждем...")
+            time.sleep(1)
+            result = get_groups(api, id, fields)
+        else:
+            result = err
+    return result
+
+
+def get_friends_groups(api, friend_id):
+    try:
+        result = api.groups.get(user_id=friend_id)
+    except Exception as err:
+        if err.code == 6:
+            print("Ждем...")
+            time.sleep(1)
+            result = get_friends_groups(api, friend_id)
+        else:
+            result = err
     return result
 
 
@@ -19,12 +60,10 @@ def get_groups_without_friends(friends, api, groups):
             continue
         print("{} из {}".format(i, friends['count']))
         i += 1
-        try:
-            friend_groups = api.groups.get(user_id=friend['id'])
-        except:
-            print('Не удалось получить группы друга: {} {}'.format(friend['first_name'], friend['last_name']))
+        friend_groups = get_friends_groups(api, friend['id'])
+        if isinstance(friend_groups, vk.exceptions.VkAPIError):
+            print(friend_groups)
             continue
-        time.sleep(2)
         result = list(set(result) - set(friend_groups['items']))
         #if i == 10:
             #break
@@ -45,16 +84,24 @@ def get_data_about_groups(groups, groups_without_friends):
     return result
 
 
-id = 5030613
-api = get_api()
-groups = api.groups.get(user_id=id, extended=1, fields='name', count=1000)
-time.sleep(2)
-groups_id = []
-for group in groups['items']:
-    groups_id.append(group['id'])
-friends = api.friends.get(user_id=id, fields='deactivated')
-groups_without_friends = get_groups_without_friends(friends, api, groups_id)
-data_about_groups = get_data_about_groups(groups['items'], groups_without_friends)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(current_dir, 'groups.json'), 'w', encoding='utf-8') as file:
-    json.dump(data_about_groups, file)
+def get_file():
+    id = 5030613
+    api = get_api()
+    groups = get_groups(api, id, 'name')
+    if isinstance(groups, vk.exceptions.VkAPIError):
+        print(groups)
+        return
+    groups_id = []
+    for group in groups['items']:
+        groups_id.append(group['id'])
+    friends = get_friends(api, id, 'deactivated')
+    if isinstance(friends, vk.exceptions.VkAPIError):
+        print(groups)
+        return
+    groups_without_friends = get_groups_without_friends(friends, api, groups_id)
+    data_about_groups = get_data_about_groups(groups['items'], groups_without_friends)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(current_dir, 'groups.json'), 'w', encoding='utf-8') as file:
+        json.dump(data_about_groups, file)
+
+get_file()
